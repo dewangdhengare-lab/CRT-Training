58e943b200096515c6ceea184e71c4e3f04a5dca
Dewang Dhengare <dewangdhengare@gmail.com>
2026-05-25 00:52:46 +0530

    Delete P6.py

--- P6.py
+++
@@ -1,8 +0,0 @@
-#Accept 9 digit number and find sum of 1st and last digit (in 3 steps)
-Number = int(input('Enter number'))
-n1 = Number//100000000
-n2 = Number%10
-
-print(n1+n2)
-# result = n1
-# print(result)
